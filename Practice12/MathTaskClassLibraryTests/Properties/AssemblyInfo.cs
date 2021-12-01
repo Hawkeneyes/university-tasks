97dcaf51b545cedc1768f36ce9823fbf391c04b5
TAHK235 <mr.lomomakin@mail.ru>
2021-12-01 20:12:07 +0300

    add Lesson11,12

---
+++ Practice12/MathTaskClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathTaskClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathTaskClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("181bdc3a-e259-41ab-b335-18bff1b024dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Practice12/TranspositionCipher/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TranspositionCipher
+{
+    public partial class Form1 : Form
+    {
+        Transposition t;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            t = new Transposition();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            t.SetKey(keyTextBox.Text);
+
+            if (encryptRadioButton.Checked)
+                outputTextBox.Text = t.Encrypt(inputTextBox.Text);
+            else
+                outputTextBox.Text = t.Decrypt(inputTextBox.Text);
+        }
+    }
+}
